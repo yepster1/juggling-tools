d16c03fed2d3f50544c4d1c766d4fe1fa5b7eb85
Cary Small <csmall@taivara.com>
2021-11-17 00:36:52 +0200

    created function to turn a string into a siteswap object

--- src/siteSwap.ts
+++ src/siteSwap.ts
@@ -13,7 +13,27 @@ export interface juggle {
 }
 
 export function createSiteSwapFromString(str: string): siteSwap {
-  return null;
+  str = stripUnimportantCharacters(str);
+  var Juggles: juggle[] = [];
+  var splits = str.split("x");
+  for (var i = 0; i < splits.length; i++) {
+    var sequence = splits[i].split("");
+    for (var n = 0; n < sequence.length; n++) {
+      var juggle: juggle = { siteSwap: parseInt(sequence[n]) };
+      if (i < splits.length - 1 && n == sequence.length - 1) {
+        juggle.cross = "x";
+      }
+      Juggles.push(juggle);
+    }
+  }
+  return { sequence: Juggles };
+}
+
+function stripUnimportantCharacters(str: string) {
+  str = str.replace(/\(/g, "");
+  str = str.replace(/\)/g, "");
+  str = str.replace(/,/g, "");
+  return str;
 }
 
 //function to take a siteSwap as an input and return whether it is possible
@@ -38,6 +58,6 @@ export function shiftLeft(siteSwap: siteSwap): siteSwap {
   return null;
 }
 
-export function shifRight(siteSwap: siteSwap): siteSwap {
+export function shiftRight(siteSwap: siteSwap): siteSwap {
   return null;
 }
--- tst/siteSwap.ts
+++ tst/siteSwap.ts
@@ -1,15 +1,11 @@
 import { expect } from "chai";
-import {
-  createSiteSwapFromString,
-  siteSwap,
-  juggle,
-} from "../src/siteSwapToHand";
+import { createSiteSwapFromString, siteSwap, juggle } from "../src/siteSwap";
 
 describe("createSiteSwapFromString", function () {
   it("correctly creates a siteswap from a valid string", function () {
     const expectedResult: siteSwap = { sequence: [{ siteSwap: 3 }] };
     let result = createSiteSwapFromString("3");
-    expect(result).equal(expectedResult);
+    expect(JSON.stringify(result)).equal(JSON.stringify(expectedResult));
   });
 
   it("correctly creates a siteswap from a valid string", function () {
@@ -17,7 +13,7 @@ describe("createSiteSwapFromString", function () {
       sequence: [{ siteSwap: 4 }, { siteSwap: 4 }],
     };
     let result = createSiteSwapFromString("44");
-    expect(result).equal(expectedResult);
+    expect(JSON.stringify(result)).equal(JSON.stringify(expectedResult));
   });
 
   it("correctly creates a siteswap from a valid sync string", function () {
@@ -25,6 +21,19 @@ describe("createSiteSwapFromString", function () {
       sequence: [{ siteSwap: 4, cross: "x" }, { siteSwap: 4 }],
     };
     let result = createSiteSwapFromString("4x4");
-    expect(result).equal(expectedResult);
+    expect(JSON.stringify(result)).equal(JSON.stringify(expectedResult));
+  });
+
+  it("correctly creates a siteswap from a valid sync string", function () {
+    const expectedResult: siteSwap = {
+      sequence: [
+        { siteSwap: 4, cross: "x" },
+        { siteSwap: 4 },
+        { siteSwap: 4, cross: "x" },
+        { siteSwap: 4 },
+      ],
+    };
+    let result = createSiteSwapFromString("(4x4)(4x4)");
+    expect(JSON.stringify(result)).equal(JSON.stringify(expectedResult));
   });
 });
