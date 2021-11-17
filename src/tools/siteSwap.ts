5bf0e669581acc8ec6dbef092e4c86862a04de2b
Cary Small <csmall@taivara.com>
2021-11-17 04:41:34 +0200

    fixed warnings

--- src/tools/siteSwap.ts
+++ src/tools/siteSwap.ts
@@ -20,7 +20,7 @@ export function createSiteSwapFromString(str: string): siteSwap {
     var sequence = splits[i].split("");
     for (var n = 0; n < sequence.length; n++) {
       var juggle: juggle = { siteSwap: parseInt(sequence[n]) };
-      if (i < splits.length - 1 && n == sequence.length - 1) {
+      if (i < splits.length - 1 && n === sequence.length - 1) {
         juggle.cross = "x";
       }
       Juggles.push(juggle);
@@ -43,12 +43,12 @@ export function isSiteSwapPossible(siteSwap: siteSwap): boolean {
 
 //function to take a siteSwap and convert it to the throws each hand makes
 export function siteSwapToHands(siteSwap: siteSwap): hands {
-  if (siteSwap.sequence.length % 2 == 1) {
+  if (siteSwap.sequence.length % 2 === 1) {
     throw new Error("siteSwap must have an even length");
   }
   var hands: hands = { left: [], right: [] };
   for (var i = 0; i < siteSwap.sequence.length; i++) {
-    if (i % 2 == 0) {
+    if (i % 2 === 0) {
       hands.left.push(siteSwap.sequence[i]);
     } else {
       hands.right.push(siteSwap.sequence[i]);
@@ -80,38 +80,38 @@ export function convertStringToSync(str: string): string[] {
 export function shiftLeft(siteSwap: siteSwap): siteSwap {
   const hands: hands = siteSwapToHands(siteSwap);
   for (var i = 0; i < hands.left.length; i++) {
-    if (hands.left[i].siteSwap % 2 == 1) {
+    if (hands.left[i].siteSwap % 2 === 1) {
       hands.left[i].siteSwap--;
       hands.left[i].cross = "x";
     }
   }
-  for (var i = 0; i < hands.right.length; i++) {
-    if (hands.right[i].siteSwap % 2 == 1) {
+  for (i = 0; i < hands.right.length; i++) {
+    if (hands.right[i].siteSwap % 2 === 1) {
       hands.right[i].siteSwap++;
       hands.right[i].cross = "x";
     }
   }
   return handsToSiteSwap(hands);
 }
-function siteSwapHands(siteSwap: siteSwap): siteSwap {
-  const hands: hands = siteSwapToHands(siteSwap);
-  const temp = hands.left;
-  hands.left = hands.right;
-  hands.right = temp;
-  return handsToSiteSwap(hands);
-}
+// function siteSwapHands(siteSwap: siteSwap): siteSwap {
+//   const hands: hands = siteSwapToHands(siteSwap);
+//   const temp = hands.left;
+//   hands.left = hands.right;
+//   hands.right = temp;
+//   return handsToSiteSwap(hands);
+// }
 
 export function shiftRight(siteSwap: siteSwap): siteSwap {
   siteSwap.sequence.unshift(siteSwap.sequence.pop() as juggle);
   const hands: hands = siteSwapToHands(siteSwap);
   for (var i = 0; i < hands.left.length; i++) {
-    if (hands.left[i].siteSwap % 2 == 1) {
+    if (hands.left[i].siteSwap % 2 === 1) {
       hands.left[i].siteSwap++;
       hands.left[i].cross = "x";
     }
   }
-  for (var i = 0; i < hands.right.length; i++) {
-    if (hands.right[i].siteSwap % 2 == 1) {
+  for (i = 0; i < hands.right.length; i++) {
+    if (hands.right[i].siteSwap % 2 === 1) {
       hands.right[i].siteSwap--;
       hands.right[i].cross = "x";
     }
@@ -129,7 +129,7 @@ export function siteSwapToString(siteSwap: siteSwap): string {
     }
   }
   if (cross) {
-    for (var i = 0; i < siteSwap.sequence.length - 1; i += 2) {
+    for (i = 0; i < siteSwap.sequence.length - 1; i += 2) {
       str +=
         "(" +
         siteSwap.sequence[i].siteSwap +
@@ -140,7 +140,7 @@ export function siteSwapToString(siteSwap: siteSwap): string {
         ")";
     }
   } else {
-    for (var i = 0; i < siteSwap.sequence.length; i++) {
+    for (i = 0; i < siteSwap.sequence.length; i++) {
       str += siteSwap.sequence[i].siteSwap;
     }
   }
